e2bd5db82baabe3a47bbd7e80b2f5a64c0c534b5
tresor <tshims79@gmail.com>
2023-06-19 17:04:33 +0200

    Add project files.

---
+++ TimesTableFunction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+namespace TimesTableFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunTimesTable();
+        }
+
+        private static void RunTimesTable()
+        {
+            throw new NotImplementedException();
+        }
+
+        static void TimesTable(byte number)
+        {
+            WriteLine($"This is the {number} times table:");
+        }
+    }
+}
